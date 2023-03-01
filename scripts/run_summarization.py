ae4d121bc231cb8119de7efc3b6acea6819a842e
johngiorgi <johnmgiorgi@gmail.com>
2023-03-01 10:19:24 -0500

    fix outstanding issues with new metrics

--- scripts/run_summarization.py
+++ scripts/run_summarization.py
@@ -695,7 +695,7 @@ def main():
     exact_match = load_metric("exact_match", cache_dir=model_args.cache_dir)
     rouge = load_metric("rouge", cache_dir=model_args.cache_dir)
     bertscore = load_metric("bertscore", cache_dir=model_args.cache_dir)
-    # bleurt = load_metric("bleurt", "BLEURT-20", cache_dir=model_args.cache_dir)
+    bleurt = load_metric("bleurt", "BLEURT-20", cache_dir=model_args.cache_dir)
 
     def postprocess_text(preds, labels):
         preds = [sanitize_text(pred) for pred in preds]
@@ -786,7 +786,6 @@ def main():
         result.update({"bleurt": np.mean(bleurt_result["scores"]).item()})
 
         # Compute an ensemble score for the generations
-        result["ensemble_gen_score"] = np.mean(result["rouge_avg"], result["bertscore_f1"], result["bleurt"]).item()
         result["ensemble_gen_score"] = np.mean([result["rouge_avg"], result["bertscore_f1"], result["bleurt"]]).item()
         result = {k: round(v * 100, 4) for k, v in result.items()}
 
