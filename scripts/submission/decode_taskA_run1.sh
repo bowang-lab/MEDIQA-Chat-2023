a25fbcd26cd844fa2a5b05a4b09686f0e33c3208
johngiorgi <johnmgiorgi@gmail.com>
2023-03-12 11:54:22 -0400

    Use batch size of one, disable mixed precision

--- scripts/submission/decode_taskA_run1.sh
+++ scripts/submission/decode_taskA_run1.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+# Submits our Flan-T5 large based approach for task A
 
 TEST_FP="$1"  # Provided to the script by the submission system
 
 python3 ./scripts/run_summarization.py "./conf/base.yml" "./conf/task_a.yml" \
     test_file="$TEST_FP" \
     output_dir="./output/wanglab/task_a/run_1" \
+    per_device_eval_batch_size=1 \
+    bf16=false \
     do_train=false \
     do_eval=false \
     do_predict=true
--- scripts/submission/decode_taskB_run1.sh
+++ scripts/submission/decode_taskB_run1.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+# Submits our LED large based approach for task B
 
 TEST_FP="$1"  # Provided to the script by the submission system
 
 python3 ./scripts/run_summarization.py "./conf/base.yml" "./conf/task_b.yml" \
     test_file="$TEST_FP" \
     output_dir="./output/wanglab/task_b/run_1" \
+    per_device_eval_batch_size=1 \
+    fp16=false \
     do_train=false \
     do_eval=false \
     do_predict=true
