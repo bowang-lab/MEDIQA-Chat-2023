d76ccb4b6674e29ebcb0ab3d3753a012c5a05a99
johngiorgi <johnmgiorgi@gmail.com>
2023-03-16 11:17:06 -0400

    Update slurm submission script

--- scripts/slurm/run_summarization.sh
+++ scripts/slurm/run_summarization.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 # Requested resources
-#SBATCH --mem=8G
+#SBATCH --mem=16G
 #SBATCH --cpus-per-task=1
 #SBATCH --gres=gpu:a100:1
 # Wall time and job details
 #SBATCH --time=1:00:00
 #SBATCH --job-name=baseline
-#SBATCH --account=def-wanglab-ab_gpu
+#SBATCH --account=def-gbader
 # Emails me when job starts, ends or fails
 #SBATCH --mail-user=johnmgiorgi@gmail.com
 #SBATCH --mail-type=FAIL
 # Use this command to run the same job interactively
-# salloc --mem=8G --cpus-per-task=1 --gres=gpu:a100:1 --time=3:00:00 --account=def-wanglab-ab_gpu
-# salloc --mem=8G --cpus-per-task=1 --gres=gpu:a100:1 --time=3:00:00 --account=def-gbader
+# salloc --mem=16G --cpus-per-task=1 --gres=gpu:a100:1 --time=3:00:00 --account=def-wanglab-ab_gpu
+# salloc --mem=16G --cpus-per-task=1 --gres=gpu:a100:1 --time=3:00:00 --account=def-gbader
 
 ### Example usage ###
-# sbatch "./scripts/slurm/run_summarization.sh" "./conf/task_a.yml"
+# sbatch "./scripts/slurm/run_summarization.sh" "./conf/task_a.yml" "./output/task_a"
 
 ### Usage notes ###
 # The amount of time needed will depend on the batch size, model and number of GPUs requested.
@@ -24,6 +24,8 @@
 ### Environment ###
 # Add your W&B key here to enable W&B reporting (or login with wandb login)
 # export WANDB_API_KEY=""
+# Note: If running on a node without internet access, sync your runs to W&B after the job finishes by calling:
+# wandb sync -p mediqa-2023 -e wang-lab --sync-all
 
 module purge  # suggested in alliancecan docs: https://docs.alliancecan.ca/wiki/Running_jobs
 module load python/3.10 StdEnv/2020 gcc/9.3.0 arrow/7.0.0
@@ -40,12 +42,11 @@ OUTPUT_DIR="$2"       # The path on disk to save the output to
 ### Job ###
 # This calls a modified version of the example summarization script from HF (with Trainer). For details,
 # see: https://github.com/huggingface/transformers/tree/main/examples/pytorch/summarization#with-trainer
+# Note: it is better to cache models, metrics and datasets to $SCRATCH, as it is much faster and larger than $HOME.
 WANDB_MODE=offline \
 TRANSFORMERS_OFFLINE=1 \
 HF_DATASETS_OFFLINE=1 \
-python ./scripts/run_summarization.py "./conf/base.yml" "$CONFIG_FILEPATH" \
-    output_dir="$OUTPUT_DIR"
+python ./scripts/run_summarization.py "./conf/base.yml" "$CONFIG_FILEPATH" output_dir="$OUTPUT_DIR" \
+    cache_dir="$SCRATCH/.cache/huggingface" overwrite_output_dir=true
 
 exit
-
-# WANDB_MODE=offline TRANSFORMERS_OFFLINE=1 HF_DATASETS_OFFLINE=1 python ./scripts/run_summarization.py "./conf/base.yml" "./conf/task_b.yml" output_dir="./output/a_first" overwrite_output_dir=true summary_column="section_text" train_file="./datasets/MEDIQA-Chat-Training-ValidationSets-Feb-10-2023/TaskA/TaskA-TrainingSet.csv" validation_file="./datasets/MEDIQA-Chat-Training-ValidationSets-Feb-10-2023/TaskA/TaskA-ValidationSet.csv" test_file="./datasets/MEDIQA-Chat-Training-ValidationSets-Feb-10-2023/TaskA/TaskA-ValidationSet.csv" per_device_train_batch_size=8 gradient_accumulation_steps=1 eval_steps=250 save_steps=250 eval_delay=6000 max_train_samples=250
