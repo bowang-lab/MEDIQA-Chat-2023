8256dd31d58dec79952d22ab2e5af7c6de94e68f
johngiorgi <johnmgiorgi@gmail.com>
2023-03-16 10:59:17 -0400

    Name output folders in submission scripts

--- scripts/submission/decode_taskA_run1.sh
+++ scripts/submission/decode_taskA_run1.sh
@@ -5,7 +5,7 @@ TEST_FP="$1"  # Provided to the script by the submission system
 
 python3 ./scripts/run_summarization.py "./conf/base.yml" "./conf/task_a.yml" \
     test_file="$TEST_FP" \
-    output_dir="./output/wanglab/task_a/run_1" \
+    output_dir="./outputs/wanglab/task_a/run_1" \
     per_device_eval_batch_size=1 \
     bf16=false \
     do_train=false \
