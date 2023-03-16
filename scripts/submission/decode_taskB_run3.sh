1d52baab111f881c356a5494d332bf5494208cd8
johngiorgi <johnmgiorgi@gmail.com>
2023-03-16 19:06:08 -0400

    Add submission script for openai

--- scripts/submission/decode_taskB_run3.sh
+++ scripts/submission/decode_taskB_run3.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# Submits our LLM (via LangChain) based approach for task B
+# Submits our LLM (via OpenAI) based approach for task B
 
 TEST_FP="$1"  # Provided to the script by the submission system
 
 # Notes:
 # - You must provide an OPENAI_API_KEY for this to work
 OPENAI_API_KEY="..." \
-python3 ./scripts/run_langchain.py "$TEST_FP" "./outputs/wanglab/taskB/run2" --task "B" --run "2"
+python3 ./scripts/run_openai.py "$TEST_FP" "./outputs/wanglab/taskB/run3" --task "B" --run "3"
