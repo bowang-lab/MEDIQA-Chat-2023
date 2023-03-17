7e0add20729c5600c25aa91fa900c5d47acd17a0
johngiorgi <johnmgiorgi@gmail.com>
2023-03-17 17:28:50 -0400

    Add example usage

--- scripts/postprocess_taskB.py
+++ scripts/postprocess_taskB.py
@@ -3,6 +3,11 @@ from datasets import load_dataset
 
 
 def main(submission_fp: str = typer.Argument("Filepath (or URL) to the submission file (should be a CSV file).")):
+    """Postprocesses the submission file for Task B.
+
+    Example usage:
+    python scripts/postprocess_taskB.py "./outputs/taskB_wanglab_run1.csv"
+    """
     test = load_dataset(
         "csv",
         data_files={
