3ad4ead114dfcdd5fead4a8f3354ea3ad17e12fc
johngiorgi <johnmgiorgi@gmail.com>
2023-03-21 11:04:39 -0400

    Update openAI key

--- scripts/submission/decode_taskB_run2.sh
+++ scripts/submission/decode_taskB_run2.sh
@@ -5,7 +5,7 @@ TEST_FP="$1"  # Provided to the script by the submission system
 
 # Notes:
 # - You must provide an OPENAI_API_KEY for this to work
-OPENAI_API_KEY="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" \
+OPENAI_API_KEY="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" \
 python3 ./scripts/run_langchain.py "./data/MEDIQA-Chat-Training-ValidationSets-Feb-10-2023/TaskB/TaskB-TrainingSet.csv" \
     "$TEST_FP" \
     "./outputs" \
