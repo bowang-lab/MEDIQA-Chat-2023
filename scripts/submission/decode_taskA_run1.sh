ccfbfd90f5e07ab267dc458c0333b477b05043d8
johngiorgi <johnmgiorgi@gmail.com>
2023-03-21 20:03:08 -0400

    Fix issues in taskA/B run 1 submission files

--- scripts/submission/decode_taskA_run1.sh
+++ scripts/submission/decode_taskA_run1.sh
@@ -5,20 +5,25 @@ TEST_FP="$1"  # Provided to the script by the submission system
 
 # Notes:
 # - The model will be downloaded from the HuggingFace model hub
-# - The script expects a summary column in the test file, but we don't have one, so use the ID column
-# - Use the run=1 argument to ensure that the output file is named correctly
+# - The script expects a summary column in the test file, but we don't have one, so use the dialogue column
+# - The script expects a validation file, but we don't have one, so use the test file
 # - Set the batch size to one and turn off all mixed precision to avoid errors
-python3 ./scripts/run_summarization.py "./conf/base.yml" "./conf/taskA.yml" \
-    test_file="$TEST_FP" \
+# - Set the bertscore_model_type and bleurt_checkpoint to null to avoid running them
+# - Use the run=1 argument to ensure that the output file is named correctly
+python3 ./scripts/run_summarization.py "./conf/base.yml" "./conf/taskA.yml" output_dir="./outputs" \
     model_name_or_path="wanglab/task-a-flan-t5-large" \
-    summary_column="ID" \
-    run="1" \
-    output_dir="./outputs" \
+    summary_column="dialogue" \
+    train_file=null \
+    validation_file="$TEST_FP" \
+    test_file="$TEST_FP" \
     per_device_eval_batch_size=1 \
     fp16=false \
     bf16=false \
     do_train=false \
     do_eval=false \
-    do_predict=true
+    do_predict=true \
+    bertscore_model_type=null \
+    bleurt_checkpoint=null \
+    run="1"
 
 python3 ./scripts/submission_checker.py "./outputs/taskA_wanglab_run1.csv"
