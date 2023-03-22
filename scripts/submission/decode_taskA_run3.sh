d842de849e2c972e68e8844f815826433527be6f
johngiorgi <johnmgiorgi@gmail.com>
2023-03-22 19:16:17 -0400

    Add run3 submission for task A

---
+++ scripts/submission/decode_taskA_run3.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Submits our Flan-T5 large based approach for task A
+
+TEST_FP="$1"  # Provided to the script by the submission system
+
+# Notes:
+# - The model will be downloaded from the HuggingFace model hub
+# - The script expects a summary column in the test file, but we don't have one, so use the dialogue column
+# - The script expects a validation file, but we don't have one, so use the test file
+# - Set the batch size to one and turn off all mixed precision to avoid errors
+# - Set the bertscore_model_type and bleurt_checkpoint to null to avoid running them
+# - Use the run=3 argument to ensure that the output file is named correctly
+python3 ./scripts/run_summarization.py "./conf/base.yml" "./conf/taskA.yml" output_dir="./outputs" \
+    model_name_or_path="wanglab/task-a-flan-t5-large-run-3" \
+    summary_column="dialogue" \
+    train_file=null \
+    validation_file="$TEST_FP" \
+    test_file="$TEST_FP" \
+    per_device_eval_batch_size=1 \
+    fp16=false \
+    bf16=false \
+    do_train=false \
+    do_eval=false \
+    do_predict=true \
+    bertscore_model_type=null \
+    bleurt_checkpoint=null \
+    run="3"
+
+# Validate submission
+python3 ./scripts/submission_checker.py "./outputs/taskA_wanglab_run3.csv"
